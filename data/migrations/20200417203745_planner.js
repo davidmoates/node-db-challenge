4feba08f1fa39b1c0ab3e502bf41c18f5d695948
David Moates <david.m.moates@gmail.com>
2020-04-17 21:11:47 -0500

    Started project

---
+++ data/migrations/20200417203745_planner.js
@@ -0,0 +1,42 @@
+
+exports.up = async function(knex) {
+  await knex.schema.createTable("projects", table => {
+      table.increments("id")
+      table.text("name").notNull()
+      table.text("description").notNull()
+      table.boolean("is_complete")
+    })
+
+    await knex.schema.createTable("task", table => {
+      table.increments("id")
+      table.text("description").notNull()
+      table.text("notes")
+      table.integer("project_id").notnullable()
+            .references("id")
+            .inTable("projects")
+      table.boolean("is_complete")
+    })
+
+    await knex.schema.createTable("resources", table => {
+      table.increments("id")
+      table.text("resource_name").notNull().unique()
+      table.description()
+    })
+
+    await knex.schema.createTable("projects_resources", table => {
+      table.integer("project_id")
+            .references("id")
+            .inTable("projects")
+      table.integer("resource_id")
+      .references("id")
+      .inTable("resources")
+      table.primary({"project_id", "resource_id"})
+    })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists("projects_resources")
+  await knex.schema.dropTableIfExists("resources")
+  await knex.schema.dropTableIfExists("task")
+  await knex.schema.dropTableIfExists("projects")
+};
